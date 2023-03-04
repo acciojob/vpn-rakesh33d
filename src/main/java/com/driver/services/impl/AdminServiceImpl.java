3aed5b4ae249489721a79f5da27011008861fce2
Rakesh <rakesh33d@gmail.com>
2023-03-04 13:00:16 +0530

    admin service

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,5 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
@@ -20,13 +24,65 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin = new Admin();
+        //set attributes before saving
+        admin.setUsername(username);
+        admin.setPassword(password);
+
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+        Admin admin = adminRepository1.findById(adminId).get();
+        //set attributes
+        ServiceProvider serviceProvider = new ServiceProvider();
+        serviceProvider.setName(providerName);
+        //update the list of service providers in admin
+        admin.getServiceProviders().add(serviceProvider);
+
+        adminRepository1.save(admin);//child will automatically be saved
+        return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+
+        if(countryName.equals("IND") ||
+                countryName.equals("USA") ||
+                countryName.equals("AUS") ||
+                countryName.equals("CHI") ||
+                countryName.equals("JPN") ){
+
+            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+            Country country = new Country();
+            if(countryName.equals("IND")){
+               country.setCountryName(CountryName.IND);
+               country.setCode(CountryName.IND.toCode());
+            }
+            if(countryName.equals("USA")){
+                country.setCountryName(CountryName.USA);
+                country.setCode(CountryName.USA.toCode());
+            }
+            if(countryName.equals("AUS")){
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            if(countryName.equals("CHI")){
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            if(countryName.equals("JPN")){
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+            serviceProviderRepository1.save(serviceProvider);
+            return serviceProvider;
+        }else{
+            throw new Exception("Country not found");
+        }
+
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.driver.services.impl;
 
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
