90f063f08ed6d820cdc8bda95afe310feb0a33cd
Rakesh <rakesh33d@gmail.com>
2023-03-04 21:11:03 +0530

    changes in adminService

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -25,9 +25,8 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public Admin register(String username, String password) {
         Admin admin = new Admin();
-        //set attributes before saving
-        admin.setUsername(username);
         admin.setPassword(password);
+        admin.setUsername(username);
 
         adminRepository1.save(admin);
         return admin;
@@ -36,53 +35,51 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
         Admin admin = adminRepository1.findById(adminId).get();
-        //set attributes
         ServiceProvider serviceProvider = new ServiceProvider();
-        serviceProvider.setName(providerName);
-        //update the list of service providers in admin
-        admin.getServiceProviders().add(serviceProvider);
 
-        adminRepository1.save(admin);//child will automatically be saved
+        serviceProvider.setAdmin(admin);
+        serviceProvider.setName(providerName);
+
+        admin.getServiceProviders().add(serviceProvider);
+        adminRepository1.save(admin);
+
         return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-
-        if(countryName.equals("IND") ||
-                countryName.equals("USA") ||
-                countryName.equals("AUS") ||
-                countryName.equals("CHI") ||
-                countryName.equals("JPN") ){
-
-            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+        if(countryName.equalsIgnoreCase("ind") || countryName.equalsIgnoreCase("usa") || countryName.equalsIgnoreCase("aus")||countryName.equalsIgnoreCase("jpn")||countryName.equalsIgnoreCase("chi")){
 
             Country country = new Country();
-            if(countryName.equals("IND")){
-               country.setCountryName(CountryName.IND);
-               country.setCode(CountryName.IND.toCode());
+            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+            if (countryName.equalsIgnoreCase("ind")){
+                country.setCountryName(CountryName.IND);
+                country.setCode(CountryName.IND.toCode());
             }
-            if(countryName.equals("USA")){
+            if (countryName.equalsIgnoreCase("usa")){
                 country.setCountryName(CountryName.USA);
                 country.setCode(CountryName.USA.toCode());
             }
-            if(countryName.equals("AUS")){
+            if (countryName.equalsIgnoreCase("aus")){
                 country.setCountryName(CountryName.AUS);
                 country.setCode(CountryName.AUS.toCode());
             }
-            if(countryName.equals("CHI")){
-                country.setCountryName(CountryName.CHI);
-                country.setCode(CountryName.CHI.toCode());
-            }
-            if(countryName.equals("JPN")){
+            if (countryName.equalsIgnoreCase("jpn")){
                 country.setCountryName(CountryName.JPN);
                 country.setCode(CountryName.JPN.toCode());
             }
+            if (countryName.equalsIgnoreCase("chi")){
+                country.setCountryName(CountryName.CHI);
+                country.setCode(CountryName.CHI.toCode());
+            }
+            country.setServiceProvider(serviceProvider);
+            serviceProvider.getCountryList().add(country);
             serviceProviderRepository1.save(serviceProvider);
-            return serviceProvider;
-        }else{
-            throw new Exception("Country not found");
-        }
 
+            return serviceProvider;
+        }
+        else
+            throw new Exception("Country not found");
     }
 }
